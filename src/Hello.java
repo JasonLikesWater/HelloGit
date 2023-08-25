ff61920814f686dec3fd73f09c10b88e6d0f4bb1
Gavin <Gavin@GavinsPC.unl.edu>
2023-08-25 10:19:33 -0500

    Added MY name and major to the code

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,8 @@ public class Hello {
 	 public static void main(String args[]) {
 		    System.out.println("Jason Le");
 		    System.out.println("Computer Engineering");
+		    System.out.prinln("Gavin Lane");
+		    System.out.println("Computer Science");
 		  }
 	 
 }
